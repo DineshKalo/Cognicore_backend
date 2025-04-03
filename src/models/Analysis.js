2c134b6312f2d48054236d00b8aba3e14a9e0277
DineshKalo <124802914+DineshKalo@users.noreply.github.com>
2025-04-03 14:12:09 +0530

    Analysis updated

--- src/models/Analysis.js
+++ src/models/Analysis.js
@@ -4,14 +4,16 @@ const Interview = require("./Interview");
 
 const Analysis = sequelize.define("Analysis", {
   id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-  correctness_score: DataTypes.FLOAT,
-  fluency_score: DataTypes.FLOAT,
   alert_count: DataTypes.INTEGER,
+  details: DataTypes.JSONB,
+
   interview_id: {  // Foreign Key
     type: DataTypes.INTEGER,
     references: { model: 'interviews', key: "id" },
     onDelete: "CASCADE",
   },
+  score:DataTypes.FLOAT,
+  status: DataTypes.STRING,
 },
 {
   tableName: 'analysis',
